0cf9c9d2d312dc02be1ef4bb3a34af42ffaf2ab1
Grace Kostanich <fl5812597@gmail.com>
2022-09-13 16:13:18 -0700

    Add reducers
    Co-authored-by: Seung Lee <leark@users.noreply.github.com>

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,83 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
+
+  const currentState = {
+    1: {
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1 
+    }, 2: {
+      names: 'Jasmine and Justine',
+      location: '2a',
+      issue: 'Reducer has side effects.',
+      id: 2 
+    }
+  }
+
+  let action;
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new ticket data to mainTicketList', () => {
+    const { names, location, issue, id } = ticketData; //destructuring (reassign properties of ticketData )
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    //The code above is action, it is passed into our ticketListReducer below as action 
+    expect(ticketListReducer(currentState, action)).toEqual({   //ticketListReducer is our "reducer" from "ticket-list-reducer"
+
+                                                                //currentState is defined above as:   const currentState = {
+                                                                //   1: {
+                                                                //     names: 'Ryan & Aimen',
+                                                                //     location: '4b',
+                                                                //     issue: 'Redux action is not working correctly.',
+                                                                //     id: 1 
+                                                                //       },
+                                                                //
+                                                                //   2: {
+                                                                //     names: 'Jasmine and Justine',
+                                                                //     location: '2a',
+                                                                //     issue: 'Reducer has side effects.',
+                                                                //     id: 2 
+                                                                //   }
+                                                                // }
+
+      2: {
+        names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 
+      }
+    });
   });
 });
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,6 +1,70 @@
+/*
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+*/
+
 const reducer = (state = {}, action) => {
-  return state;
-};
+  const { names, location, issue, id } = action;
+  switch (action.type) {
+    //this switch case is saying if action.type is = to any case, do what comes after the : (*on the same line as case*)
+  case 'ADD_TICKET':
+    //Object.assign is taking in three arguments: Arguments listed below (v)
+    //1: An empty object so it can pass in our "state" object (action) to that empty object we created (this is so it doesn't mutate the state)
+    //2: This argument, "state", is the one that will be cloned (aka the one we are passing into the empty object we have instantiated above) 
+    //3: Our last argument is what changes we want to make to our new object. This will be the new ticket that will eventually get added to our ticket 
+    return Object.assign({}, state, {
+      [id]: { //putting id inside of [] brackets lets id = the variable id (which is equal to action.id form our action object)
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+    case 'DELETE_TICKET':
+      let newState = { ...state }; //here we make a copy of the state object and store it in a variable called newState (which we will be mutating)
+      delete newState[id]; // here we delete the id of the newState (which is a copy ) 
+                           // [id] is actually [action.id] if we hadn't destructured on line 12
+      return newState;
+    default:
+      return state;
+    }
+  };
+  
+  export default reducer;
 
-export default reducer;
+    // const id = 1
+    // newState.id -> id: { ??? }
+    // newState[id] -> 1: { ??? } 
 
+
+    //newState = {
+    //   1: {
+    //     names: 'Ryan & Aimen',
+    //     location: '4b',
+    //     issue: 'Redux action is not working correctly.',
+    //     id: 1 
+    //       },
+    //
+    //   2: {
+    //     names: 'Jasmine and Justine',
+    //     location: '2a',
+    //     issue: 'Reducer has side effects.',
+    //     id: 2 
+    //   }
+    // }
+
+    // newState.1
+    // newState[1]
+
+    // newState[2] ==== 
+   //   2: {
+    //     names: 'Jasmine and Justine',
+    //     location: '2a',
+    //     issue: 'Reducer has side effects.',
+    //     id: 2 
+    //   }
